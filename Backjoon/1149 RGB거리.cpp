9d2fbf76d57ba12edf6572c617e53fe9506378be
9503chl <9503chl@naver.com>
2021-04-02 15:13:40 +0900

    1149 RGB거리

---
+++ Backjoon/1149 RGB거리.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct stt {
+	int a1, a2, a3;
+};
+int main() {
+	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(0);
+	int N, rst = 0; cin >> N;
+	vector <stt> dp(N + 1, { 0, });
+	cin >> dp[1].a1 >> dp[1].a2 >> dp[1].a3; //26 40 83
+	for (int i = 2; i <= N; i++) {
+		cin >> dp[i].a1 >> dp[i].a2 >> dp[i].a3;
+		dp[i].a1 = dp[i].a1 + min(dp[i - 1].a2, dp[i - 1].a3);
+		dp[i].a2 = dp[i].a2 + min(dp[i - 1].a1, dp[i - 1].a3);
+		dp[i].a3 = dp[i].a3 + min(dp[i - 1].a2, dp[i - 1].a1);
+		rst = min(dp[i].a1, min(dp[i].a2, dp[i].a3));
+	}
+	cout << rst;
+}
