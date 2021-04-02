4cc5eed1f59be4d0b314b8858a1128f834ddbcee
9503chl <9503chl@naver.com>
2021-04-02 15:11:17 +0900

    2225 합분해

--- Backjoon/2225 합분해.cpp
+++ Backjoon/2225 합분해.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-struct stt {
-	int a1, a2, a3;
-};
+
 int main() {
 	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(0);
-	int N, rst = 0; cin >> N; 
-	vector <stt> dp(N+1, { 0, });
-	cin >> dp[1].a1 >> dp[1].a2 >> dp[1].a3; //26 40 83
-	for (int i = 2; i <= N; i++) {
-		cin >> dp[i].a1 >> dp[i].a2 >> dp[i].a3;
-		dp[i].a1 = dp[i].a1 + min(dp[i - 1].a2, dp[i - 1].a3);
-		dp[i].a2 = dp[i].a2 + min(dp[i - 1].a1, dp[i - 1].a3);
-		dp[i].a3 = dp[i].a3 + min(dp[i - 1].a2, dp[i - 1].a1);
-		rst = min(dp[i].a1, min(dp[i].a2, dp[i].a3));
+	int N, K, cnt = 0; cin >> N >> K;
+	vector <vector <long long>> dp(K + 1, vector <long long>(N + 1));
+	if (K == 1) cout << 1;
+	else if (K == 2) cout << N + 1;
+	else {
+		for (int i = 0; i <= N; i++) dp[2][i] = 1;
+		for (int i = 3; i <= K; i++) {
+			for (int j = 0; j <= N; j++) {
+				if (j == 0) dp[i][j] = 1;
+				else {
+					dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000000;
+				}
+			}
+		}
+		for (int i = 0; i <= N; i++) {
+			cnt = (cnt + dp[K][i]) % 1000000000;
+		}
+		cout << cnt % 1000000000;
 	}
-	cout << rst;
 }
