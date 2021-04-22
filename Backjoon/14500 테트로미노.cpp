fd50995e0d4ba1960a4d9a842c8c330b3c3aa2a3
9503chl <9503chl@naver.com>
2021-04-22 22:53:52 +0900

    14500 노가다문제 안풀어~

--- Backjoon/14500 테트로미노.cpp
+++ Backjoon/14500 테트로미노.cpp
@@ -1,10 +1,79 @@
 #include <iostream>
-#include <vector>
-#include <math.h>
-using namespace std;
-int main()
-{
-	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(0);
-	int N, M;
-	cin >> N >> M;
+#define max_int 501
+using namespace  std;
+
+int n, m, a[max_int][max_int], result;
+bool check[max_int][max_int];
+
+
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { -1, 1, 0, 0 };
+
+int ex[4][4] = { {0, 0, 0, 1}, {0, 1, 2, 1}, {0, 0, 0, -1}, {0, -1, 0, 1} };
+int ey[4][4] = { {0, 1, 2, 1}, {0, 0, 0, 1}, {0, 1, 2, 1}, {0, 1, 1, 1} };
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+void dfs(int x, int y, int sum_value, int length) {
+    if (length >= 4) {
+        result = max(result, sum_value);
+        return;
+    }
+
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (nx < 1 || nx > n || ny < 1 || ny > m) continue;
+        if (!check[nx][ny]) {
+            check[nx][ny] = true;
+            dfs(nx, ny, sum_value + a[nx][ny], length + 1);
+            check[nx][ny] = false;
+        }
+    }
+}
+void check_exshape(int x, int y) {
+
+    for (int i = 0; i < 4; i++) {
+
+        bool isOut = false;
+        int sum_value = 0;
+
+        for (int j = 0; j < 4; j++) {
+            int nx = x + ex[i][j];
+            int ny = y + ey[i][j];
+
+            if (nx < 1 || nx > n || ny < 1 || ny > m) {
+                isOut = true;
+                break;
+            }
+            else {
+                sum_value += a[nx][ny];
+            }
+        }
+        if (!isOut) {
+            result = max(result, sum_value);
+        }
+    }
+}
+
+int main() {
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            check[i][j] = true;
+            dfs(i, j, a[i][j], 1);
+            check[i][j] = false;
+            check_exshape(i, j);
+        }
+    }
+    printf("%d\n", result);
 }
