eda1ea8a8eee093c6dbc1c1651a74585a1645d6d
9503chl <9503chl@naver.com>
2021-04-27 23:59:50 +0900

    ,.

---
+++ Backjoon/13023 ABCDE.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(0);
+	int N; cin >> N;
+	
+}
