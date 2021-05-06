025cd5c867ae38637cf9a4a379ecc924a31084cb
9503chl <9503chl@naver.com>
2021-05-06 14:22:40 +0900

    1107 21.05.06

--- Backjoon/1107 리모컨.cpp
+++ Backjoon/1107 리모컨.cpp
@@ -2,43 +2,37 @@
 #include <vector>
 #include <math.h>
 using namespace std;
-//이방법말고 전부탐색하느 방법을 찾아보자 아직 몾찾았따
-int N, M; vector <int> but(10, true);
-int min1 = 0, length = 0, cnt = 0, rst = 0, cntB = 0;
-void check(int a) {
-	a %= 10;
-	for (int i = 0; i < 10; i++)
-		if (but[i] == true) {
-			min1 = abs(a - i); break; //시작
-		}
-	for (int i = 0; i < 10; i++)
-		if (but[i] == true) min1 = min(min1, abs(a - i));
-	for (int i = 0; i < 10; i++) {
-		if (abs(a - i) == min1 && but[i] == true) {
-			cnt = i; break; //인덱스찾기
-		}
+
+vector <bool> but(10, true);
+int check(int a) {
+	if (a == 0) {
+		if (!but[0]) return 0;
+		else return 1;
 	}
-	cntB++;
+	int len = 0;
+	while (a > 0) {
+		if (!but[a % 10]) return 0;
+		a /= 10;
+		len++;
+	}
+	return len;
 }
 int main()
 {
 	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(0);
-	cin >> N >> M;
-	for (int i = 0; i < M; i++) { //123456
+	int N, M, rst = 0; cin >> N >> M;
+	for (int i = 0; i < M; i++) { 
 		int num; cin >> num;
 		but[num] = false;
 	}
-	if (N == 100) cout << 0;
-	else {
-		int x = N;
-		while (x / 10) {
-			x /= 10;
-			length += 1;
+	rst = abs(N - 100);
+	for (int i = 0; i <= 1000000; i++) {
+		int b = i;
+		int len = check(b);
+		if (len > 0) {
+			int cnt = abs(b - N);
+			if (rst > cnt + len) rst = cnt + len;
 		}
-		for (int i = length; i >= 0; i--) {
-			check(N / pow(10, i));
-			rst += (cnt * pow(10, i));
-		}
-		cout << min(abs(N - rst) + cntB, abs(N - 100));
 	}
+	cout << rst << "\n";
 }
