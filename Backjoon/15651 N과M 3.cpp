4f9ffd6a7929b51f06028189caf420d94910d927
9503chl <9503chl@naver.com>
2021-05-20 23:42:18 +0900

    21.05.20 자살

---
+++ Backjoon/15651 N과M 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void Bt(int Array[],int n, int m) {
+	for (int i = 1; i <= n; i++) {
+		int num = m-1;
+		while (Array[num] > n) {
+			for (int j=0; j < num; j++)
+		}
+	}
+}
+int main()
+{
+	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(0);
+	int N, M; cin >> N >> M; int cnt[7] = { 1, };
+	Bt(cnt,N, M);
+}
