bda61b086663c29a5059e3408d5bd2e61890163b
9503chl <9503chl@naver.com>
2021-05-22 22:04:44 +0900

    15651 N과M 3

--- Backjoon/15651 N과M 3.cpp
+++ Backjoon/15651 N과M 3.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-using namespace std;
 
-void Bt(int Array[],int n, int m) {
-	for (int i = 1; i <= n; i++) {
-		int num = m-1;
-		while (Array[num] > n) {
-			for (int j=0; j < num; j++)
+using namespace std;
+int Array[8];
+void Bt(int n, int m, int cnt){ //n은 최대수, m은 길이
+	for (int i = 0; i < n; i++) { //재귀를 이용하자
+		Array[cnt] = i + 1;
+		if (cnt < m-1) Bt(n, m, cnt + 1);
+		else {
+			for (int j = 0; j < m; j++) cout << Array[j] << " ";
+			cout << "\n";
 		}
 	}
 }
 int main()
 {
 	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(0);
-	int N, M; cin >> N >> M; int cnt[7] = { 1, };
-	Bt(cnt,N, M);
+	int N, M, a; cin >> N >> M;
+	for (int i = 0; i < M; i++) Array[i] = 1;
+	Bt(N, M, 0);
 }
