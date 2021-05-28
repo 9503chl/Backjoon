dd6dab15425d356e7c52d2f7359c1db25f23e9aa
9503chl <9503chl@naver.com>
2021-05-28 11:09:13 +0900

    14501 퇴사

--- Backjoon/14501 퇴사.cpp
+++ Backjoon/14501 퇴사.cpp
@@ -7,13 +7,14 @@ int main()
 {
 	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(0);
 	int N, max1 = 0; cin >> N;
-	vector <int> dp(N + 1, 0),rst(N+1,0), day(N + 1, 0);
-	for (int i = 1; i <= N; i++) cin >> dp[i] >> day[i];
+	vector <int> dp(N + 1, 0),rst(N+2,0), day(N + 1, 0);
+	for (int i = 1; i <= N; i++) cin >> day[i] >> dp[i];
 	for (int i = 1; i <= N; i++) {
-		if (i + day[i] < N) {
-			rst[i+day[i]] = rst[i] + max(rst[i+day[i]], dp[i]);
+		rst[i] = max(rst[i], rst[i - 1]);
+		if (i + day[i] <= N+1) {
+			rst[i+day[i]] = max(rst[i] + dp[i], rst[i+day[i]]);
 		}
 	}
-	for (int i = 1; i <= N; i++) max1 = max(max1, rst[i]);
+	for (int i = 1; i <= N+1; i++) max1 = max(max1, rst[i]);
 	cout << max1;
 }
