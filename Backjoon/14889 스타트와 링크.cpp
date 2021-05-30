05c672ec21284e3727cccd1c48ae8d4b9c17f867
9503chl <9503chl@naver.com>
2021-05-30 21:44:52 +0900

    14889 스타트와 링크

--- Backjoon/14889 스타트와 링크.cpp
+++ Backjoon/14889 스타트와 링크.cpp
@@ -4,45 +4,41 @@
 using namespace std;
 int team[20][20] = { {0,0}, };
 int min1 = 101;
-vector <int> rst;
 vector <bool> yes;
 int cnt1 = 0, cnt2 = 0;
-void cnt_sum(int n) { // 12  34
-	for (int i = 0; i < n / 2; i++) {
-		for (int j = i + 1; j < n / 2; j++) {
-			cnt1 += team[rst[i]][rst[j]] + team[rst[j]][rst[i]];
+void Bt(int n,int cnt, int num) {
+	if (num == n / 2) {
+		vector <int> link, start;
+		for (int i = 0; i < n; i++) {
+			if (yes[i]) start.push_back(i);
+			else link.push_back(i);
 		}
-	}
-	for (int i = n / 2; i < n; i++) {
-		for (int j = i + 1; j < n; j++) {
-			cnt2 += team[rst[i]][rst[j]] + team[rst[j]][rst[i]];
-		}
-	}
-}
-void Bt(int n, int num) {
-	for (int i = 0; i < n; i++) { //01 23 
-		if (num < n) {
-			if (!yes[i]) {
-				rst[num] = i;
-				yes[i] = true;
-				Bt(n, num+1);
-				yes[i] = false;
+		for (int i = 0; i < start.size(); i++) {
+			for (int j = i + 1; j < link.size(); j++) {
+				int str_x = start[i], str_y = start[j];
+				int lik_x = link[i], lik_y = link[j];
+				cnt1 += team[str_x][str_y] + team[str_y][str_x];
+				cnt2 += team[lik_x][lik_y] + team[lik_y][lik_x];
 			}
-			else continue;
 		}
-		else {
-			cnt_sum(n); min1 = min(min1, abs(cnt1 - cnt2));
-			break;
+		min1 = min(min1, abs(cnt1 - cnt2));
+		cnt1 = 0; cnt2 = 0;
+	}
+	for (int i = cnt+1; i < n; i++) { //01 23
+		if (!yes[i]) {
+			yes[i] = true;
+			Bt(n, i,num+1);
+			yes[i] = false;
 		}
 	}
 }
 int main()
 {
-	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(0);
-	int N; cin >> N; rst.resize(N,0), yes.resize(N, false);
+	cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+	int N; cin >> N; yes.resize(N, false);
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) cin >> team[i][j];
 	}
-	Bt(N,0);
+	Bt(N,0,0);
 	cout << min1;
 }
