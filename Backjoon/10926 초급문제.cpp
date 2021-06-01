b8321e6e27812ae1c7d29edad170b355a2f8c6e0
9503chl <9503chl@naver.com>
2021-06-01 13:18:59 +0900

    10926 초급문제

---
+++ Backjoon/10926 초급문제.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+int main()
+{
+	cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+	string str;
+	cin >> str; cout << str + "??!";
+}
