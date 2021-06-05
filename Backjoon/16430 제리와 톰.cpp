b0daa5ba1df86c2eb153bec47830c9f3d7536e5c
9503chl <9503chl@naver.com>
2021-06-05 18:33:12 +0900

    15962, 16430

---
+++ Backjoon/16430 제리와 톰.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
+	int A, B;
+	cin >> A >> B;
+	cout << B - A << " " << B;
+}
