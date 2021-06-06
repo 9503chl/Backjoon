61b50fbf4f118976fdc80e75a4cc191f163288bc
9503chl <9503chl@naver.com>
2021-06-06 20:17:46 +0900

    14645 비와이

---
+++ Backjoon/14645 비와이.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
+	int N, K,A,B;
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) {
+		cin >> A >> B;
+	}
+	cout << "ºñ¿ÍÀÌ";
+}
