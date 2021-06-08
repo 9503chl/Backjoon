0d2d47827c2b8576e653ebf83acd7ff48bd43aa3
9503chl <9503chl@naver.com>
2021-06-08 23:00:08 +0900

    14652 행복

--- Backjoon/14652 행복.cpp
+++ Backjoon/14652 행복.cpp
@@ -5,5 +5,5 @@ using namespace std;
 int main() {
 	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
 	int N,M,K; cin >> N >> M >> K;
-	cout << K % N + 1 << " " << K % M + 1;
+	cout << K / M << " " << K % M;
 }
