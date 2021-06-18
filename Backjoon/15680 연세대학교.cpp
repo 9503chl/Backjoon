f08c129e063af8d9854de2ea26984250d95d208d
9503chl <9503chl@naver.com>
2021-06-18 12:33:42 +0900

    15680 연세대학교

---
+++ Backjoon/15680 연세대학교.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+#include <vector> 
+#include <cmath>
+using namespace std;
+
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);; cin.tie(0);
+	int N; cin >> N;
+	if (N) cout << "Leading the Way to the Future";
+	else cout << "YONSEI";
+}
