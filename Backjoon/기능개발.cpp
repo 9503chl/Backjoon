a6df87a7272c7af76ac667d6c8fc6490ad284cbe
9503chl <9503chl@naver.com>
2021-07-02 23:28:13 +0900

    기능개발 - 문자열

--- Backjoon/기능개발.cpp
+++ Backjoon/기능개발.cpp
@@ -1,25 +1,17 @@
 #include <string> 
 #include <vector> 
-#include <stack>
 #include <iostream> 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
-    vector<int> answer; int cnt = 0; int cur = 1; int size1 = 0;
-    stack <int> stk; //321 54
-    while (cur <= progresses.size()) {
-        for (int i = 0; i < size(progresses); i++) {
-            if (progresses[i] >= 100) {
-                stk.push(i + 1); //순서 저장
-                progresses[i] -= 10001; 
-            }
-            progresses[i] += speeds[i];
+    vector<int> answer; int cnt = 0;
+    while (progresses.size() !=0) {
+        for (int i = 0; i < size(progresses); i++) progresses[i] += speeds[i];
+        while (!progresses.empty() && !speeds.empty() && progresses[0] >= 100) {
+            cnt++;
+            speeds.erase(speeds.begin()), progresses.erase(progresses.begin());
         }
-        if (stk.top() == cur) {
-            size1 = stk.size();
-            for (int i = 0; i < size1; i++) cnt+1, stk.pop();
-            answer.push_back(cnt), cur += cnt, cnt = 0; 
-        }
+        if (cnt != 0) answer.push_back(cnt),cnt = 0;
     }
     return answer;
 }
